8d9a06dbd2a4a733520b54b49e9b25f141eef048
IhorKravtsov <kravtsov.igor.dev@gmail.com>
2025-05-18 20:50:23 +0200

    Update import path for CSS in main.tsx to reflect new directory structure

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 
 import { App } from "./_/app.tsx";
 
-import "./index.css";
+import "./_/index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
