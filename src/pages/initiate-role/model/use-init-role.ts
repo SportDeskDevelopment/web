ec6035d5e8defa8d9fb1e93c9b91ac43ad180319
IhorKravtsov <kravtsov.igor.dev@gmail.com>
2025-05-21 10:31:43 +0200

    Refactor role initialization logic in useInitRole hook
    - Simplified the isRoleInitialized condition by removing the traineeProfile check, focusing on trainerProfile and parentProfile for role initialization.

--- src/pages/initiate-role/model/use-init-role.ts
+++ src/pages/initiate-role/model/use-init-role.ts
@@ -3,10 +3,7 @@ import { userApi } from "@/shared/api/user";
 export const useInitRole = () => {
   const user = userApi.useGetCurrentUserSuspense();
 
-  const isRoleInitialized =
-    user.data.traineeProfile ||
-    user.data.trainerProfile ||
-    user.data.parentProfile;
+  const isRoleInitialized = user.data.trainerProfile || user.data.parentProfile;
 
   return {
     isInitialized: isRoleInitialized,
